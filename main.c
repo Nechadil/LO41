7aaeed920cea9ad86e888551387e712e2481d7ab
Nechadil <lidachen21@gmail.com>
2016-05-26 13:57:56 +0200

    Add files via upload

--- main.c
+++ main.c
@@ -7,12 +7,11 @@ int numtrain=0;
 int nb_tgv_oe=5;
 
 pthread_t tgv[5];
-pthread_mutex_t tunnel;
+pthread_mutex_t P0;
 pthread_mutex_t num_mut;
-pthread_mutex_t voie_a2;
-pthread_mutex_t voie;
-pthread_mutex_t garageTGV;
-pthread_mutex_t voie_c;
+pthread_mutex_t P1;
+pthread_mutex_t P2;
+pthread_mutex_t P3;
 
 
 
@@ -23,40 +22,40 @@ void *tgv_oe( )
 	numtrain++;
 	pthread_mutex_unlock(&num_mut);
 	
-	pthread_mutex_lock(&voie_c);
-	pthread_mutex_lock(&voie_a2);
-	pthread_mutex_lock(&garageTGV);
-	printf ("tgv %d est entré et attend dans la gare\n",num);
+	pthread_mutex_lock(&P0);
+	pthread_mutex_lock(&P1);
+	printf ("TGV_OE%d attend\n",num);
 	sleep(1);
-	printf("tgv %d est parti vers la garage\n",num);
-	
+	printf("TGV_OE%d départ Gare\n",num);
 	sleep(1);
-	printf("tgv %d est dans la garage\n",num);
+	printf(" %28s%d %s\n","TGV_OE",num,"arrive Garage");
 	
-	pthread_mutex_lock(&tunnel);
-	pthread_mutex_lock(&voie);
+	pthread_mutex_unlock(&P0);
+	pthread_mutex_unlock(&P1);
 	
 	
-	printf("tgv %d est parti travers le tunnel\n",num);
+	printf(" %28s%d %s\n","TGV_OE",num,"départ Garage");
 	
-	pthread_mutex_unlock(&voie_c);	
-	pthread_mutex_unlock(&voie_a2);
-	pthread_mutex_unlock(&garageTGV);
+	pthread_mutex_lock(&P2);	
+	pthread_mutex_lock(&P3);
 	sleep(1);
 	
-	printf("tgv %d est sorti la gare est\n",num);
-	pthread_mutex_unlock(&tunnel);
-	pthread_mutex_unlock(&voie);
+	printf(" %53s%d %s\n","TGV_OE",num,"dans Tunnel");
+	sleep(1);
+	printf(" %83s%d %s\n","TGV_OE",num,"sort");
+	pthread_mutex_unlock(&P2);
+	pthread_mutex_unlock(&P3);
 	if (num==0)	pthread_exit(NULL);
 	else 
 	pthread_join(tgv[num--],NULL);
 }
 
+
 void thread_TGV_oe(void)
 {
         int temp;
                 
-        if((temp = pthread_create(&tgv[count], NULL, tgv_oe, NULL)) != 0)       //comment2
+        if((temp = pthread_create(&tgv[count], NULL, tgv_oe, NULL)) != 0)      
                 printf("echec de la création  !\n");
  	       else
 	{
@@ -74,13 +73,13 @@ int main()
 {	
 
         pthread_mutex_init(&num_mut,NULL);
-	pthread_mutex_init(&voie,NULL);
-	pthread_mutex_init(&garageTGV,NULL);
-	pthread_mutex_init(&voie_c,NULL);
-	pthread_mutex_init(&voie_a2,NULL);
-	pthread_mutex_init(&tunnel,NULL);
+	pthread_mutex_init(&P0,NULL);
+	pthread_mutex_init(&P1,NULL);
+	pthread_mutex_init(&P2,NULL);
+	pthread_mutex_init(&P3,NULL);
 	
-        printf("le projet commence\n");
+	
+        printf("%10s%25s%25s%25s\n\n", "Gare", "Garage", "Tunnel","Voie");
 	while(nb_tgv_oe>0)         
 	{
 		thread_TGV_oe();
